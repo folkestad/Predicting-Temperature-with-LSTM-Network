bdb5efd578a6e96fd948fb0fbf014e0904c25d36
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2017-10-24 13:53:19 +1300

    Update state during prediction

--- data_handler.py
+++ data_handler.py
@@ -11,8 +11,10 @@ import numpy
 from os import path, sys
 
 # change data format in file
-def change_date_format(src_file='Data/monthly_mean_global_surface_tempreratures_1880-2017.csv',                                          
-        dest_file='Data/monthly_mean_global_surface_tempreratures_1880-2017_new.csv'):
+
+
+def change_date_format(src_file='Data/monthly_mean_global_surface_tempreratures_1880-2017.csv',
+                       dest_file='Data/monthly_mean_global_surface_tempreratures_1880-2017_new.csv'):
     data = open(src_file, 'r')
     data_new = open(dest_file, 'w')
     counter = -1
@@ -22,20 +24,25 @@ def change_date_format(src_file='Data/monthly_mean_global_surface_tempreratures_
             continue
         counter += 1
         line_new = line.split(',')
-        line_new[0] = line_new[0].split('.')[0]+'-'
-        line_new[0] = line_new[0]+'0{}'.format(counter%12+1) if counter%12+1 <= 9 else line_new[0]+'{}'.format(counter%12+1)
+        line_new[0] = line_new[0].split('.')[0] + '-'
+        line_new[0] = line_new[0] + '0{}'.format(counter % 12 + 1) if counter % 12 + 1 <= 9 else line_new[
+            0] + '{}'.format(counter % 12 + 1)
         data_new.write(','.join(line_new))
     data.close()
     data_new.close()
 
 # date-time parsing function for loading the dataset
+
+
 def date_parser(x):
-    return datetime.strptime(x, '%Y-%m')
+    return datetime.strptime(x, '%Y')
 
 # frame a sequence as a supervised learning problem
+
+
 def timeseries_to_supervised(data, lag=1):
     df = DataFrame(data)
-    columns = [df.shift(i) for i in range(1, lag+1)]
+    columns = [df.shift(i) for i in range(1, lag + 1)]
     columns.append(df)
     df = concat(columns, axis=1)
     df.fillna(0, inplace=True)
@@ -43,6 +50,8 @@ def timeseries_to_supervised(data, lag=1):
     return df
 
 # create a differenced series
+
+
 def difference(dataset, interval=1):
     diff = list()
     for i in range(interval, len(dataset)):
@@ -51,10 +60,14 @@ def difference(dataset, interval=1):
     return Series(diff)
 
 # invert differenced value
+
+
 def inverse_difference(history, yhat, interval=1):
     return yhat + history[-interval]
 
 # scale train and test data to [-1, 1]
+
+
 def scale(train, test):
     # fit scaler
     scaler = MinMaxScaler(feature_range=(-1, 1))
@@ -68,6 +81,8 @@ def scale(train, test):
     return scaler, train_scaled, test_scaled
 
 # inverse scaling for a forecasted value
+
+
 def invert_scale(scaler, X, value):
     new_row = [x for x in X] + [value]
     array = numpy.array(new_row)
@@ -75,26 +90,28 @@ def invert_scale(scaler, X, value):
     inverted = scaler.inverse_transform(array)
     return inverted[0, -1]
 
-def get_data(file_name='Data/monthly_mean_global_surface_tempreratures_1880-2017_new.csv', predict_n_months=12, cuttoff_dataset=0):
+
+def get_data(file_name='Data/annual-water-use-in-new-york-cit.csv', predict_n=12, cuttoff_dataset=0):
     # load dataset
     series = read_csv(
-        filepath_or_buffer=file_name, 
-        sep=',', 
-        header=0, 
-        parse_dates=[0], 
+        filepath_or_buffer=file_name,
+        sep=',',
+        header=0,
+        parse_dates=[0],
         index_col=0,
-        usecols=[0,2], 
-        squeeze=True, 
+        usecols=[0, 1],
+        squeeze=True,
         date_parser=date_parser,
         skip_blank_lines=True,
-        skiprows=[1-1]
+        skiprows=[36]
     )
-    
-    # transform data to be stationary
-    raw_values = series.values[cuttoff_dataset:]
-    diff_values = difference(raw_values, 1)
 
-    print("Size of dataset: {}".format(len(raw_values)))
+    # transform data to be stationary
+    raw_values = series.values[
+        :-cuttoff_dataset] if cuttoff_dataset > 0 else series.values
+    print("Data cutoff: size {}".format(len(raw_values)))
+    diff_values = difference(raw_values, 1)
+    print("Data transformed: size {}".format(len(diff_values)))
 
     # transform data to be supervised learning
     supervised = timeseries_to_supervised(diff_values, 1)
@@ -102,7 +119,8 @@ def get_data(file_name='Data/monthly_mean_global_surface_tempreratures_1880-2017
     print(supervised.head())
 
     # split data into train and test-sets
-    train, test = supervised_values[0:-predict_n_months], supervised_values[-predict_n_months:]
+    train, test = supervised_values[
+        0:-predict_n], supervised_values[-predict_n:]
 
     # transform the scale of the data
     scaler, train_scaled, test_scaled = scale(train, test)
--- lstm_temp.py
+++ lstm_temp.py
@@ -18,122 +18,165 @@ from matplotlib import pyplot
 import numpy
 
 from data_handler import get_data, invert_scale, inverse_difference
- 
+
 # fit an LSTM network to training data
-def fit_lstm(train, batch_size, nb_epoch, neurons, hidden_layers):
+
+
+def fit(train, batch_size, epochs, neurons, hidden_layers):
     X, y = train[:, 0:-1], train[:, -1]
     X = X.reshape(X.shape[0], 1, X.shape[1])
     model = Sequential()
-    for i in range(0, hidden_layers):
-        if i == 0 and hidden_layers <= 1:
+
+    for i in range(1, hidden_layers + 1):
+        if i == 1 and hidden_layers <= 1:
+            print(1, i)
             model.add(LSTM(
-                neurons, 
+                neurons,
                 batch_input_shape=(batch_size, X.shape[1], X.shape[2]),
-                stateful=True # Means that the LSTM remember from the last batch,
+                stateful=True  # Means that the LSTM remember from the last batch,
             ))
-        elif i == 0 and hidden_layers > 1:
+        elif i == 1 and hidden_layers > 1:
+            print(2, i)
             model.add(LSTM(
-                neurons, 
+                neurons,
                 batch_input_shape=(batch_size, X.shape[1], X.shape[2]),
                 return_sequences=True,
-                stateful=True # Means that the LSTM remember from the last batch,
+                stateful=True  # Means that the LSTM remember from the last batch,
             ))
-        elif 1 < i and i < hidden_layers-1:
+        elif i > 1 and i < hidden_layers:
+            print(3, i)
             model.add(LSTM(
                 neurons,
                 return_sequences=True,
-                stateful=True # Means that the LSTM remember from the last batch
+                stateful=True  # Means that the LSTM remember from the last batch
+            ))
+        elif i == hidden_layers:
+            print(4, i)
+            model.add(LSTM(
+                neurons,
+                stateful=True  # Means that the LSTM remember from the last batch
             ))
         else:
-            model.add(LSTM(
-                neurons,
-                stateful=True # Means that the LSTM remember from the last batch
-            ))
+            print(5, "Error")
     model.add(Dense(1))
-    model.compile(loss='mean_absolute_error', optimizer='adam')
-    for i in range(nb_epoch):
-        print(i,"/",nb_epoch)
-        model.fit(X, y, epochs=1, batch_size=batch_size, verbose=1, shuffle=False)
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    for i in range(epochs):
+        print(i, "/", epochs)
+        model.fit(X, y, epochs=1, batch_size=batch_size,
+                  verbose=1, shuffle=False)
         model.reset_states()
     return model
- 
+
 # make a one-step forecast
-def forecast_lstm(model, batch_size, X):
+
+
+def forecast(model, batch_size, X):
     X = X.reshape(1, 1, len(X))
     yhat = model.predict(X, batch_size=1)
-    return yhat[0,0]
+    return yhat[0, 0]
 
-#######################################################################################
+##########################################################################
 
 # Set how many years we want to predict and convert the years to months
-n_years = 1
-n_months = 8
-n_months_total = n_years*12 + n_months
-cuttoff_dataset = 1620
+n_total = 30
+cuttoff_dataset = 0
 
 # get data sets from data handler
 scaler, raw_values, train_scaled, test_scaled = get_data(
-    file_name='Data/monthly_mean_global_surface_tempreratures_1880-2017_new.csv', 
-    predict_n_months=n_months_total,
+    file_name='Data/high-and-low-water-levels-of-the-amazon-at-iquitos-1962-1978.csv',
+    predict_n=n_total,
     cuttoff_dataset=cuttoff_dataset
 )
 
+print("Train size and Test size: {} - {}".format(len(train_scaled), len(test_scaled)))
+
 n_rounds = 1
-epochs = 1000
-neurons = 2
+epochs = 1
+neurons = 1
 hidden_layers = 1
 batch_size = 1
 rmses = []
 maes = []
+# for epochs range(10, epochs+1, 10)
 for n in range(n_rounds):
-    print("Round {} (epochs -> {}, neurons -> {} and hidden layers -> {})".format(n+1, epochs, neurons, hidden_layers))
+    print("Round {} (epochs -> {}, neurons -> {} and hidden layers -> {})".format(n +
+                                                                                  1, epochs, neurons, hidden_layers))
     # fit the model
-    lstm_model = fit_lstm(
-        train=train_scaled, 
-        batch_size=batch_size, 
-        nb_epoch=epochs, 
+    lstm_model = fit(
+        train=train_scaled,
+        batch_size=batch_size,
+        epochs=epochs,
         neurons=neurons,
         hidden_layers=hidden_layers
     )
 
     # forecast the entire training dataset to build up state for forecasting
-    train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
-    lstm_model.predict(train_reshaped, batch_size=batch_size)
-    
+    train_and_predicted = train_scaled[:, 0]
+    lstm_model.predict(train_and_predicted.reshape(
+        len(train_and_predicted), 1, 1), batch_size=batch_size)
+
     # walk-forward validation on the test data
     predictions = list()
     for i in range(len(test_scaled)):
-        # make one-step forecast
+        # get next element in test set
         X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
-        yhat = forecast_lstm(lstm_model, 1, X)
+
+        # make one-step forecast
+        yhat = forecast(model=lstm_model, batch_size=1, X=X)
+
+        # build up new state for next prediction
+        print(train_and_predicted.shape)
+        train_and_predicted = numpy.append(train_and_predicted, yhat)
+        lstm_model.reset_states()
+        lstm_model.predict(train_and_predicted.reshape(
+            len(train_and_predicted), 1, 1), batch_size=batch_size)
+
         # invert scaling
-        yhat = invert_scale(scaler, X, yhat)
+        yhat = invert_scale(scaler=scaler, X=X, value=yhat)
         # invert differencing
-        yhat = inverse_difference(raw_values, yhat, len(test_scaled) - i)
+        yhat = inverse_difference(
+            history=raw_values, yhat=yhat, interval=len(test_scaled) - i)
         # store forecast
         predictions.append(yhat)
-        expected = raw_values[len(train_scaled) + i + 1] 
-        print('Month=%d, Predicted=%f, Expected=%f, difference=%f' % (i + 1, yhat, expected, yhat-expected))
-    
+        expected = raw_values[len(train_scaled) + i + 1]
+        print('Month=%d, Predicted=%f, Expected=%f, difference=%f' %
+              (i + 1, yhat, expected, yhat - expected))
+
     # report performance
-    rmse = sqrt(mean_squared_error(raw_values[-n_months_total:], predictions))
-    mae = mean_absolute_error(raw_values[-n_months_total:], predictions)
+    rmse = sqrt(mean_squared_error(raw_values[-n_total:], predictions))
+    mae = mean_absolute_error(raw_values[-n_total:], predictions)
     print('Test RMSE: %.3f' % rmse)
     print('Test MAE: %.3f' % mae)
     rmses.append(rmse)
     maes.append(mae)
 
-    if True==True:
+    if True == True:
+
         # line plot of observed vs predicted
-        true_values = raw_values[-n_months_total:]
+        true_values = raw_values[-n_total - 1:]
         pyplot.plot(true_values)
-        pyplot.plot(predictions)
+        predictions = numpy.asarray(predictions)
+        preceding = raw_values[-n_total - 1:-n_total]
+        pyplot.plot(numpy.hstack((preceding, predictions)))
+        pyplot.show()
+
+        # second plot
+        real_values = raw_values
+        predictions = numpy.asarray(predictions)
+        preceding = real_values[:-len(predictions)]
+        pyplot.plot(real_values)
+        pyplot.plot(numpy.hstack((preceding, predictions)))
+        minimum = min(true_values)
+        maximum = max(true_values)
+        pyplot.plot((len(preceding) - 1, len(preceding) - 1),
+                    (minimum - 5, maximum + 5), 'r-')
         pyplot.show()
 
 print(rmses)
 print(maes)
-print("Avg RMSE with {} epochs: {}".format(epochs, sum(rmses)/n_rounds))
-print("Avg MAE with  {} epochs: {}".format(epochs, sum(maes)/n_rounds))
+print("Avg RMSE with {} epochs: {}".format(epochs, sum(rmses) / n_rounds))
+print("Avg MAE with  {} epochs: {}".format(epochs, sum(maes) / n_rounds))
 
 # if True==False:
 #     real_values = raw_values[-200:]
